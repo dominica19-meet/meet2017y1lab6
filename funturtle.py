043b7bfee985162de896c766f227dd5350025fd7
dominica19-meet <dominica19@meet.mit.edu>
2017-07-31 15:37:51 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,16 @@
+import turtle
+#turtle.shape('turtle')
+
+triangle=turtle.clone()
+triangle.shape('triangle')
+#triangle.goto(0,100)
+#triangle.goto(100,0)
+#triangle.goto(0,0)
+
+turtle.shape('turtle')
+square=turtle.clone()
+square.shape('triangle')
+square.goto(0,100)
+square.goto(100,0)
+square.stamp()
+square.goto(0,0)
